c7619f6c77dfe31ea7a88923e4e4d06f77f7b09c
Vincent Barrielle <vincent.barrielle@m4x.org>
2020-01-22 23:03:42 +0100

    Let the user fill-in transaction details before adding it

--- src/gui_iced.rs
+++ src/gui_iced.rs
@@ -1,11 +1,12 @@
 //! GUI based on the `iced` library.
 
 use iced::{
-    button, text_input, Button, Column, Element, Sandbox, Settings, Text,
-    TextInput,
+    button, text_input, Button, Column, Element, Radio, Row, Sandbox, Settings,
+    Text, TextInput,
 };
 
 use crate::accounts::{ParseError, ParsedAccounts};
+use crate::rational::rational_from_str;
 
 pub fn run() {
     Accounts::run(Settings::default());
@@ -16,13 +17,23 @@ struct Accounts {
     accounts: ParsedAccounts,
     last_error: Option<ParseError>,
     new_user: String,
+    new_user_btn_state: button::State,
     new_user_state: text_input::State,
     new_purchase_btn_state: button::State,
+    new_transaction_descr: String,
+    new_transaction_descr_state: text_input::State,
+    new_transaction_uid: usize,
+    new_transaction_creditor: String,
+    new_transaction_amount: String,
+    new_transaction_amount_state: text_input::State,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     NewUserStrChange(String),
+    NewDescrStrChange(String),
+    UserSelected(usize),
+    NewAmountStrChange(String),
     AddUser,
     AddPurchase,
 }
@@ -40,17 +51,40 @@ impl Sandbox for Accounts {
 
     fn update(&mut self, message: Message) {
         self.last_error = match message {
-            Message::AddUser => self.accounts.add_user(self.new_user.clone()),
+            Message::AddUser => {
+                let mut new_user = String::new();
+                std::mem::swap(&mut self.new_user, &mut new_user);
+                self.accounts.add_user(new_user)
+            }
             Message::NewUserStrChange(new_user) => {
                 self.new_user = new_user;
+                Ok(())
+            }
+            Message::UserSelected(uid) => {
+                self.new_transaction_creditor =
+                    self.accounts.users()[uid].to_string();
+                self.new_transaction_uid = uid;
+                Ok(())
+            }
+            Message::NewDescrStrChange(new_descr) => {
+                self.new_transaction_descr = new_descr;
+                Ok(())
+            }
+            Message::NewAmountStrChange(new_amount) => {
+                if new_amount.len() == 0
+                    || rational_from_str(&new_amount).is_ok()
+                {
+                    self.new_transaction_amount = new_amount;
+                }
                 Ok(())
             }
             Message::AddPurchase => self
                 .accounts
                 .add_purchase(
-                    "Transaction".to_string(),
-                    self.accounts.users()[0].to_string(),
-                    0.into(),
+                    self.new_transaction_descr.clone(),
+                    self.new_transaction_creditor.clone(),
+                    // No Panic: validity of rational has been enforced by GUI
+                    rational_from_str(&self.new_transaction_amount).unwrap(),
                 )
                 .map(|_| ()),
         }
@@ -63,18 +97,54 @@ impl Sandbox for Accounts {
             column = column.push(Text::new(user.clone()));
         }
         column = column.push(
-            TextInput::new(
-                &mut self.new_user_state,
-                "New user",
-                &self.new_user,
-                Message::NewUserStrChange,
-            )
-            .on_submit(Message::AddUser),
+            Row::new()
+                .push(
+                    TextInput::new(
+                        &mut self.new_user_state,
+                        "New user",
+                        &self.new_user,
+                        Message::NewUserStrChange,
+                    )
+                    .on_submit(Message::AddUser),
+                )
+                .push(
+                    Button::new(
+                        &mut self.new_user_btn_state,
+                        Text::new("Add user"),
+                    )
+                    .on_press(Message::AddUser),
+                ),
         );
         for purchase in self.accounts.purchases() {
             column = column.push(Text::new(purchase.descr()));
         }
         if self.accounts.users().len() > 0 {
+            let mut user_choice = Column::new();
+            self.new_transaction_creditor =
+                self.accounts.users()[self.new_transaction_uid].to_string();
+            for (uid, user) in self.accounts.users().iter().enumerate() {
+                user_choice = user_choice.push(Radio::new(
+                    uid,
+                    user,
+                    Some(self.new_transaction_uid),
+                    Message::UserSelected,
+                ));
+            }
+            let row = Row::new()
+                .push(TextInput::new(
+                    &mut self.new_transaction_descr_state,
+                    "Transaction name",
+                    &self.new_transaction_descr,
+                    Message::NewDescrStrChange,
+                ))
+                .push(user_choice)
+                .push(TextInput::new(
+                    &mut self.new_transaction_amount_state,
+                    "Amount",
+                    &self.new_transaction_amount,
+                    Message::NewAmountStrChange,
+                ));
+            column = column.push(row);
             column = column.push(
                 Button::new(
                     &mut self.new_purchase_btn_state,
@@ -82,6 +152,9 @@ impl Sandbox for Accounts {
                 )
                 .on_press(Message::AddPurchase),
             );
+        }
+        if let Some(last_error) = &self.last_error {
+            column = column.push(Text::new(format!("Error: {}", last_error)));
         }
         column.into()
     }
