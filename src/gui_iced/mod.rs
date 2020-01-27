7eaf6f39819a0a2710368a05c49fb9ef6f6c4963
Vincent Barrielle <vincent.barrielle@m4x.org>
2020-01-27 13:28:44 +0100

    Extract transaction GUI into its own component
    This leads to clearer code and will ease the addition of the shares to
    the GUI, as well as letting the user modify existing transactions.

--- src/gui_iced/mod.rs
+++ src/gui_iced/mod.rs
@@ -1,12 +1,14 @@
 //! GUI based on the `iced` library.
 
 use iced::{
-    button, text_input, Button, Column, Element, Radio, Row, Sandbox, Settings,
-    Text, TextInput,
+    button, text_input, Button, Column, Element, Row, Sandbox, Settings, Text,
+    TextInput,
 };
 
 use crate::accounts::{ParseError, ParsedAccounts};
-use crate::rational::{rational_from_str, rational_to_string};
+use crate::rational::rational_to_string;
+
+mod transaction;
 
 pub fn run() {
     Accounts::run(Settings::default());
@@ -20,12 +22,7 @@ struct Accounts {
     new_user_btn_state: button::State,
     new_user_state: text_input::State,
     new_purchase_btn_state: button::State,
-    new_transaction_descr: String,
-    new_transaction_descr_state: text_input::State,
-    new_transaction_uid: usize,
-    new_transaction_creditor: String,
-    new_transaction_amount: String,
-    new_transaction_amount_state: text_input::State,
+    new_transaction: transaction::Transaction,
     #[cfg(feature = "debug")]
     latest_message: Option<Message>,
 }
@@ -33,9 +30,7 @@ struct Accounts {
 #[derive(Debug, Clone)]
 enum Message {
     NewUserStrChange(String),
-    NewDescrStrChange(String),
-    UserSelected(usize),
-    NewAmountStrChange(String),
+    NewTransaction(transaction::Message),
     AddUser,
     AddPurchase,
 }
@@ -67,49 +62,18 @@ impl Sandbox for Accounts {
                 self.new_user = new_user;
                 Ok(())
             }
-            Message::UserSelected(uid) => {
-                self.new_transaction_creditor =
-                    self.accounts.users()[uid].to_string();
-                self.new_transaction_uid = uid;
+            Message::NewTransaction(message) => {
+                self.new_transaction.update(message, self.accounts.users());
                 Ok(())
             }
-            Message::NewDescrStrChange(new_descr) => {
-                self.new_transaction_descr = new_descr;
-                Ok(())
-            }
-            Message::NewAmountStrChange(new_amount) => {
-                if new_amount.len() == 0
-                    || rational_from_str(&new_amount).is_ok()
-                {
-                    self.new_transaction_amount = new_amount;
-                }
-                Ok(())
-            }
-            Message::AddPurchase => {
-                let mut new_transaction_descr = String::new();
-                let mut new_transaction_creditor = String::new();
-                let mut new_transaction_amount = String::new();
-                std::mem::swap(
-                    &mut new_transaction_descr,
-                    &mut self.new_transaction_descr,
-                );
-                std::mem::swap(
-                    &mut new_transaction_creditor,
-                    &mut self.new_transaction_creditor,
-                );
-                std::mem::swap(
-                    &mut new_transaction_amount,
-                    &mut self.new_transaction_amount,
-                );
-                self.accounts
-                    .add_purchase(
-                        new_transaction_descr,
-                        new_transaction_creditor,
-                        // No Panic: validity of rational enforced by GUI
-                        rational_from_str(&new_transaction_amount).unwrap(),
-                    )
-                    .map(|_| ())
-            }
+            Message::AddPurchase => self
+                .accounts
+                .add_purchase(
+                    self.new_transaction.take_descr(),
+                    self.new_transaction.take_creditor(),
+                    self.new_transaction.take_amount(),
+                )
+                .map(|_| ()),
         }
         .err();
     }
@@ -159,36 +123,13 @@ impl Sandbox for Accounts {
             column = column.push(row);
         }
         if self.accounts.users().len() > 0 {
-            let mut user_choice = Column::new();
-            self.new_transaction_creditor =
-                self.accounts.users()[self.new_transaction_uid].to_string();
-            for (uid, user) in self.accounts.users().iter().enumerate() {
-                user_choice = user_choice.push(Radio::new(
-                    uid,
-                    user,
-                    Some(self.new_transaction_uid),
-                    Message::UserSelected,
-                ));
-            }
-            let row = Row::new()
-                .push(TextInput::new(
-                    &mut self.new_transaction_descr_state,
-                    "Transaction name",
-                    &self.new_transaction_descr,
-                    Message::NewDescrStrChange,
-                ))
-                .push(user_choice)
-                .push(TextInput::new(
-                    &mut self.new_transaction_amount_state,
-                    "Amount",
-                    &self.new_transaction_amount,
-                    Message::NewAmountStrChange,
-                ));
-            column = column.push(row);
-            if self.new_transaction_descr.len() > 0
-                && self.new_transaction_creditor.len() > 0
-                && self.new_transaction_amount.len() > 0
-            {
+            let can_add_transaction = self.new_transaction.is_valid();
+            column = column.push(
+                self.new_transaction
+                    .view(self.accounts.users())
+                    .map(|message| Message::NewTransaction(message)),
+            );
+            if can_add_transaction {
                 // FIXME this button is shown conditionally in part
                 // because there could be a bug where pressing this button
                 // before filling the new transaction info could prevent
---
+++ src/gui_iced/transaction.rs
@@ -0,0 +1,98 @@
+//! Widget describing a transaction being edited
+
+use iced::{text_input, Column, Element, Radio, Row, TextInput};
+
+use num_rational::Rational64;
+
+use crate::rational::rational_from_str;
+
+#[derive(Default)]
+pub struct Transaction {
+    descr: String,
+    descr_state: text_input::State,
+    uid: usize,
+    creditor: String,
+    amount: String,
+    amount_state: text_input::State,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    DescrStrChange(String),
+    UserSelected(usize),
+    AmountStrChange(String),
+}
+
+impl Transaction {
+    pub fn update(&mut self, message: Message, users: &[String]) {
+        match message {
+            Message::DescrStrChange(new_descr) => {
+                self.descr = new_descr;
+            }
+            Message::AmountStrChange(new_amount) => {
+                if new_amount.len() == 0
+                    || rational_from_str(&new_amount).is_ok()
+                {
+                    self.amount = new_amount;
+                }
+            }
+            Message::UserSelected(uid) => {
+                self.creditor = users[uid].to_string();
+                self.uid = uid;
+            }
+        }
+    }
+
+    pub fn view(&mut self, users: &[String]) -> Element<Message> {
+        let mut user_choice = Column::new();
+        self.creditor = users[self.uid].to_string();
+        for (uid, user) in users.iter().enumerate() {
+            user_choice = user_choice.push(Radio::new(
+                uid,
+                user,
+                Some(self.uid),
+                Message::UserSelected,
+            ));
+        }
+        Row::new()
+            .push(TextInput::new(
+                &mut self.descr_state,
+                "Transaction name",
+                &self.descr,
+                Message::DescrStrChange,
+            ))
+            .push(user_choice)
+            .push(TextInput::new(
+                &mut self.amount_state,
+                "Amount",
+                &self.amount,
+                Message::AmountStrChange,
+            ))
+            .into()
+    }
+
+    pub fn take_creditor(&mut self) -> String {
+        let mut creditor = String::new();
+        std::mem::swap(&mut creditor, &mut self.creditor);
+        creditor
+    }
+
+    pub fn take_amount(&mut self) -> Rational64 {
+        // No Panic: validity of rational enforced by GUI
+        let rat = rational_from_str(&self.amount).unwrap();
+        self.amount.clear();
+        rat
+    }
+
+    pub fn take_descr(&mut self) -> String {
+        let mut descr = String::new();
+        std::mem::swap(&mut descr, &mut self.descr);
+        descr
+    }
+
+    /// Checks wether this transaction contains all the information
+    /// to be added to the accounts
+    pub fn is_valid(&self) -> bool {
+        self.descr.len() > 0 && self.creditor.len() > 0 && self.amount.len() > 0
+    }
+}
